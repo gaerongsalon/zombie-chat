63e24cd1a5c1cb50a00648d5f179ba7e161f3fe5
jaeyoung.choi <lactrious@gmail.com>
2019-05-14 23:39:59 +0900

    App: Add deploy script

---
+++ frontend/app/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "${DEPLOY_TOKEN}" ]; then
+  echo "Error: Set DEPLOY_TOKEN first."
+  exit 1
+fi
+
+APP_NAME="zombie-chat"
+SERIAL="${1:-"$(date +"%Y%m%d_%H%M%S")"}"
+
+# https://github.com/yingyeothon/binary-distribution-api
+DEPLOY_SERVICE="https://api.yyt.life/d/${APP_NAME}"
+
+flutter build apk && \
+  curl -T build/app/outputs/apk/release/app-release.apk "$( \
+    curl -XPUT \
+      "${DEPLOY_SERVICE}/android/${APP_NAME}-${SERIAL}.apk" \
+      -H "X-Auth-Token: ${DEPLOY_TOKEN}" \
+    | tr -d '"')" && \
+  curl -XGET "${DEPLOY_SERVICE}?count=1" | jq -r '.platforms.android[].url'
+
