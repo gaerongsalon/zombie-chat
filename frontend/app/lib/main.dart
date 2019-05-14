7818d67a8eab72bd866653abba55a3391619d9de
jaeyoung.choi <lactrious@gmail.com>
2019-05-14 23:28:19 +0900

    App: Build up basic features

--- frontend/app/lib/main.dart
+++ frontend/app/lib/main.dart
@@ -1,111 +1,164 @@
+import 'dart:io';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+import 'env.g.dart';
+
 void main() => runApp(MyApp());
+
+const title = 'Zombie chat';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Zombie chat',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ChatPage(title: 'Zombie chat'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class ChatPage extends StatefulWidget {
+  ChatPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ChatPageState createState() => _ChatPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+const cryingBegin = '그';
+const cryingRepeat = '어';
+const cryingEnd = '워';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _ChatPageState extends State<ChatPage> {
+  final _scrollController = new ScrollController();
+  final _items = <String>[];
+  WebSocket _socket;
+  DateTime _pressedStart;
+  String _text = cryingBegin + cryingEnd;
+
+  @override
+  void initState() {
+    super.initState();
+    this._connect();
+  }
+
+  @override
+  void dispose() {
+    this._disconnect();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(children: [
+            Expanded(
+              child: Scrollbar(
+                child: ListView.builder(
+                  controller: this._scrollController,
+                  itemCount: this._items.length,
+                  itemBuilder: (BuildContext context, int index) => Padding(
+                      padding: const EdgeInsets.only(bottom: 6),
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                color: Colors.lightBlue.withAlpha(180),
+                              ),
+                              constraints: BoxConstraints(
+                                  maxWidth:
+                                      MediaQuery.of(context).size.width - 80),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8.0, horizontal: 16.0),
+                                child: Text(this._items[index]),
+                              )),
+                        ],
+                      )),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+            Container(
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.only(top: 8),
+                child: GestureDetector(
+                    onTapDown: (_) {
+                      this._pressedStart = DateTime.now();
+                      this._startCrying();
+                    },
+                    onTapUp: (_) {
+                      this._pressedStart = null;
+                      this._send(this._text);
+                    },
+                    onLongPressUp: () {
+                      this._pressedStart = null;
+                      this._send(this._text);
+                    },
+                    child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.yellow.withAlpha(180),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 16.0),
+                          child: Text(this._text),
+                        ))))
+          ]),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _connect() async {
+    this._socket = await WebSocket.connect(EnvironmentVariables.serverUrl);
+    print('connected $_socket');
+    this._socket.map((value) => value).listen((value) {
+      print('receive $value');
+      this.setState(() {
+        this._items.add(value);
+        Timer(
+            Duration(milliseconds: 200),
+            () => _scrollController
+                .jumpTo(_scrollController.position.maxScrollExtent));
+      });
+    });
+  }
+
+  void _disconnect() {
+    this._socket.close();
+    this._socket = null;
+  }
+
+  void _send(String message) {
+    print('send $message');
+    this._socket.add(message);
+  }
+
+  void _updateText(String newText) {
+    this.setState(() {
+      this._text = newText;
+    });
+  }
+
+  void _startCrying() async {
+    int repeat = 0;
+    while (this._pressedStart != null) {
+      this._updateText(cryingBegin + cryingRepeat * repeat + cryingEnd);
+      await Future.delayed(Duration(milliseconds: 200));
+      ++repeat;
+    }
+    this._updateText(cryingBegin + cryingEnd);
   }
 }
